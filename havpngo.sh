2b677235b44197aaf3801445811bd71f7713e46d
Tim Corcoran <tim@timmyc.com.au>
2014-09-12 13:33:21 +1000

    Tidy up of havpngo.sh, removing use of && and making echos more descriptive and using newlines

--- havpngo.sh
+++ havpngo.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # A simple script to deploy havpn easily
-bundle exec cap puppet:go HOSTFILTER=bpsyd07-dvmh-radius-01.inf.bulletproof.net options="--tags freeradius" role=syd07 && echo "test done, check logs, 10 second delay" && sleep 10 && echo "apply time in 2 seconds" && sleep 2 && bundle exec cap puppet:go HOSTFILTER=bpsyd07-dvmh-radius-01.inf.bulletproof.net options="--tags freeradius" role=syd07 apply=true && bundle exec cap puppet:go HOSTFILTER=bpsyd07-dvmh-radius-02.inf.bulletproof.net options="--tags freeradius" role=syd07 apply=true
+echo -e "\nThis will first do a noop run on radius-01, sleep, then do an apply run of radius-01, then 02\n"
+bundle exec cap puppet:go HOSTFILTER=bpsyd07-dvmh-radius-01.inf.bulletproof.net options="--tags freeradius" role=syd07 
+echo -e "\nNoop run on radius-01 complete. Check above output for any errors, and if needed CTRL + C in next 10 seconds\n" 
+sleep 10 
+echo -e "\nLast chance before apply run!\n" 
+sleep 2 
+echo -e "\nOk, apply time\n"
+bundle exec cap puppet:go HOSTFILTER=bpsyd07-dvmh-radius-01.inf.bulletproof.net options="--tags freeradius" role=syd07 apply=true 
+bundle exec cap puppet:go HOSTFILTER=bpsyd07-dvmh-radius-02.inf.bulletproof.net options="--tags freeradius" role=syd07 apply=true
