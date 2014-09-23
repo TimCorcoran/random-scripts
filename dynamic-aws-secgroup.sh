6724b7b2bc85335d32927ebcf65a8949295e10e5
Tim Corcoran <tim@timmyc.com.au>
2014-09-23 11:22:00 +1000

    add basics of dynamic-aws-secgroup

--- dynamic-aws-secgroup.sh
+++ dynamic-aws-secgroup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # A simple script to add your DYNAMIC external IP address to an AWS Security group
 
+# Get your current external IP address
+dig +short myip.opendns.com @resolver1.opendns.com
+
+# Example of a rule to add rule
+aws ec2 authorize-security-group-ingress --group-id sg-903004f8 --protocol tcp --port 3389 --cidr 203.0.113.0/24
+
+# Example of a rule to remove rule
+aws ec2 authorize-security-group-egress --group-id sg-903004f8 --protocol tcp --port 3389 --cidr 203.0.113.0/24
