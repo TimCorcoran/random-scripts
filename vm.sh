d631d376335a53a588c9d36d2db3a66150f17842
timc <timc@servtop.local>
2014-12-14 12:46:49 +1100

    New script to easily start/stop headless vms in virtualbox

---
+++ vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Simple script to start/stop headless vms in dropbox - By Tim Corcoran
+
+if [ "$1" = "start" ]; then
+  vboxmanage startvm $2 --type headless
+else
+
+if [ "$1" = 'stop' ]; then
+  vboxmanage controlvm $2 poweroff
+fi
+fi
+
