e42b3b19501d976eb238eee0492152efd12bb415
Tim Corcoran <tim@timmyc.com.au>
2016-02-02 08:47:54 +1100

    addition of reboot.sh

--- rdp.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-templaterdp='/Users/beltim/Dropbox/template.rdp'
-temporaryRDP='/Users/beltim/Dropbox/temp.rdp'
-command="/Applications/Microsoft Remote Desktop.app/Contents/MacOS/Microsoft Remote Desktop"
-hostname=${1}
-
-cp -f ${templaterdp} ${temporaryRDP}
-sed -i '' "s|verbarrugosa|${hostname}|g" ${temporaryRDP}
-open -na "${command}" --args ${temporaryRDP}
