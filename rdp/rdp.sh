a072d720b203ab0669c834cf97eff00704a663f6
Tim Corcoran <tim@timmyc.com.au>
2016-04-06 12:46:27 +1000

    Fixed broken

--- rdp/rdp.sh
+++ rdp/rdp.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 templaterdp='/Users/beltim/Dropbox/template.rdp'
 temporaryRDP='/Users/beltim/Dropbox/temp.rdp'
-command="/Applications/MicrosoftRemoteDesktop.app/Contents/MacOS/Microsoft Remote Desktop"
 hostname=${1}
 
 cp -f ${templaterdp} ${temporaryRDP}
 sed -i '' "s|verbarrugosa|${hostname}|g" ${temporaryRDP}
-open -na "${command}" --args ${temporaryRDP}
+open -na /Applications/Microsoft\ Remote\ Desktop.app/Contents/MacOS/Microsoft\ Remote\ Desktop --args /Users/beltim/Dropbox/temp.rdp
